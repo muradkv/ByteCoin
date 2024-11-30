028175b99dfb0879c54f7998f1b08fd0955b3cd4
murad <muradkv@gmail.com>
2024-11-30 20:55:20 +0300

    Initial commit

---
+++ ByteCoin/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  ByteCoin
+//
+//  Created by murad on 30.11.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+}
+
---
+++ ByteCoin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by murad on 30.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .brown
+    }
+
+
+}
+
